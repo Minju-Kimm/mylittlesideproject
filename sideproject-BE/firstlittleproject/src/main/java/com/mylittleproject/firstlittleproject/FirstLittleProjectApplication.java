27e36472533c873f5eb5decd9bdb42b926d793d8
Minju-Kimm <alswn299842@naver.com>
2024-09-01 19:03:01 +0900

    초기 세팅

---
+++ sideproject-BE/firstlittleproject/src/main/java/com/mylittleproject/firstlittleproject/FirstLittleProjectApplication.java
@@ -0,0 +1,17 @@
+package com.mylittleproject.firstlittleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class FirstLittleProjectApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstLittleProjectApplication.class, args);
+	}
+
+}
---
+++ sideproject-BE/firstlittleproject/src/test/java/com/mylittleproject/firstlittleproject/FirstLittleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylittleproject.firstlittleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLittleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
